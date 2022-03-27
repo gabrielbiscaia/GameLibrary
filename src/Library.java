d298824c9ec8986d5794e57b557917633fee5a9d
Gabriel Biscaia <ra118928@uem.br>
2022-03-27 17:11:58 -0300

    implementation of remove's function

--- src/Library.java
+++ src/Library.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Library {
     private Game[][] game = new Game[20][8];
     private String aux;
-    private int gameNumber = 0;
+    private int i;
+    private int numberOfGames = 0, gameDeleted, gameNumber;
     Scanner sc = new Scanner(System.in);
 
     public void addGame(){
-        gameNumber = gameNumber++;
+        numberOfGames += 1;
+        gameNumber = numberOfGames;
         System.out.println("---New Game---");
         System.out.println("Game's name: ");
         game[gameNumber][0].setName(sc.nextLine());
@@ -41,7 +43,30 @@ public class Library {
         sc.close();
     }
     public void removeGame(){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---Delete Game---");
+        System.out.println("Which game do you want to delete?");
+        for(i=0;i<numberOfGames;i++){
+            System.out.println("["+i+"]"+" "+game[i][1]);
+        }
+        System.out.println("Type here: ");
+        gameDeleted = sc.nextInt();
+        sc.close();
+        if(gameDeleted > numberOfGames){//if you type a number bigger than the quantity of games
+            System.out.println("You cannot delete this game.");
+            removeGame();
+        }else{
+            for(i = gameDeleted; i<numberOfGames; i++){
+                game[i][0] = game[(i+1)][0];
+                game[i][1] = game[(i+1)][1];
+                game[i][2] = game[(i+1)][2];
+                game[i][3] = game[(i+1)][3];
+                game[i][4] = game[(i+1)][4];
+                game[i][5] = game[(i+1)][5];
+                game[i][6] = game[(i+1)][6];
+            }
+            numberOfGames -= 1;
+        }
     }
     public void consultGames(){
 
